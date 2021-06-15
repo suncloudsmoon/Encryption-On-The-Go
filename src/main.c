4f4a8b5cb3841a7e522b2c3a2528d9a1cbf42324
suncloudsmoon <gajjam2003@gmail.com>
2021-06-14 20:56:54 -0700

    Almost Complete...

--- src/main.c
+++ src/main.c
@@ -28,7 +28,7 @@
 #include <dir.h>
 #include <windows.h>
 
-#include "include/leafgame2d/leafgame2d.h"
+#include "../include/leafgame2d/leafgame2d.h"
 
 #define APPNAME "Happy Secure"
 #define CONTEXTMENUITEM "Encrypt/Decrypt"
@@ -46,77 +46,92 @@ typedef struct {
     FILE *plainText;
 } App;
 
-// Global Prototypes
-void initApp();
-void setupHomePath(char *appName, App *app);
-void setupLogStuff(App *app);
-void setupRegistry(App *app);
-void freeApp(App *app);
 
-// Static Prototypes
+/// Global Prototypes ///
+void initApp();
+
+/// Basic Setup ///
+void setupHomePath(char *appName, App *app);
+void setupLogPath(App *app);
+
+/// Installation Algorithms ///
+void setupRegAndLogIt(App *app);
+
+/// Encryption/Decryption Algorithms ///
+char* fileToText(App *app);
+char* encryptText(char *plainText, App *app);
+char* decryptText(char *cipherText, App *app);
+void textToFile(char *cipherText, char *fileLocation, App *app);
+
+/// Static Prototypes
+static char createRegistryEntries(char optionName[], App *app);
+static int isRegFileOK(App *app);
+static bool isEncrypted(char *fileLocation, App *app);
+static char* getFirstCharFromFile(char *fileLocation, App *app);
+
+void freeApp(App *app);
 
 
 int main(char *fileLocation) {
     /// TODO: Hide the console based on the stuff in fileLocation
+    /// TODO: Create Start Menu Shortcut, a exe called "Uninstall Blah.exe"
 
+    // Allocate a block of memory for struct App
     App *app = (App *) calloc(1, sizeof(App));
 
+    // Basic Setup
     setupHomePath(APPNAME, app);
+    setupLogPath(app);
 
     if (fileLocation == NULL) {
-        // Set up the reg stuff
-        // Put up a messagebox dialog with 5 second delay for the next one that notifies the user that the app is installing and its done
+        /*
+         * Set up the reg stuff
+         * Put up a messagebox dialog with 5 second delay for the next one that notifies the user that the app is installing and its done
+         */
+        setupRegAndLogIt(app);
 
     } else {
-        // Check if the first character of the stream is E with \n following (actually read both of them); if not, then ask to encrypt
-        // via messagebox with password as input
+        /*
+         * Check if the first character of the stream is E with \n following (actually read both of them); if not, then ask to encrypt
+         * via messagebox with password as input
+         */
 
-        // First: open the fileLocation
-        char *encryptionStatus = readFileLocation(fileLocation, app);
-        if (strcmp(encryptionStatus, "E\n")) {
-            // Decrypt it
+        if (isEncrypted(fileLocation, app)) {
+            /// Decrypt the file
+            char *cipherText = fileToText(app);
+            char *plainText = decryptText(cipherText, app);
+            textToFile(plainText, fileLocation, app);
+
+            // Free Some Resources
+            free(cipherText);
+            free(plainText);
 
         } else {
-            // Encrypt it
+            /// Encrypt the file
             // Reset the position of the file pointer back to zero (Hope it works)
             fseek(app->plainText, -2, SEEK_SET);
 
+            char *plainText = fileToText(app);
+            char *cipherText = encryptText(plainText, app);
+            textToFile(cipherText, fileLocation, app);
+
+            // Frees held resources
+            free(plainText);
+            free(cipherText);
         }
     }
 
+    // Free everything else
     freeApp(app);
 
     return 0;
 }
 
-char* readFileLocation(char *fileLocation, App *app) {
-    if (access(fileLocation) == F_OK) {
-        char *input = (char *) calloc(2, sizeof(char));
+void initApp() {
 
-        app->plainText = fopen(fileLocation, "r+");
-        fgets(input, 2, app->plainText);
-        return input;
-    } else {
-        return NULL;
-    }
 }
 
-char* fileToText(App *app) {
-    // Max of 10,000 characters for now
-    char *text = (char *) calloc(10000, sizeof(char));
-
-    while (!feof(app->plainText)) {
-        char input[BUFSIZ];
-        fgets(input, BUFSIZ, app->plainText);
-        strncat(text, input, BUFSIZ);
-    }
-    return text;
-}
-
-char* encryptText(char *plainText, App *app) {
-    // typecasting char* to double = WARNING!!
-    leaf_encrypt(app->encryptKey, plainText);
-}
+/// Basic Setup ///
 
 /**
 Sets up the home path of the App
@@ -144,32 +159,66 @@ void setupLogPath(App *app) {
     snprintf(app->logPath, PATHSIZE, "%s\\%s.log", app->homePath, logName);
 }
 
+/// For Setting up the Application ///
+
 /**
 Sets up the registry stuff
 */
-void setupRegistry(App *app) {
+void setupRegAndLogIt(App *app) {
     int status = isRegFileOK(app);
 
-    if (status == false) {
-        createRegAndLogIt();
+    if (status != true) {
+        FILE *createIt = fopen(app->regPath, "w");
+        char result = createRegistryEntries(CONTEXTMENUITEM, app);
+        fprintf(createIt, "%c", result);
+
+        // Free up the resources
+        fclose(createIt);
     }
     /// TODO: if status == -1 or true, you need to handle it!
+    /// TODO: Log the status
+
 }
 
+
+/// For Encrypting/Decrypting Text ///
+
+
+char* fileToText(App *app) {
+    // Max of 10,000 characters, for now
+    char *text = (char *) calloc(10000, sizeof(char));
+
+    while (!feof(app->plainText)) {
+        char input[BUFSIZ];
+        fgets(input, BUFSIZ, app->plainText);
+        strncat(text, input, BUFSIZ);
+    }
+    return text;
+}
+
+char* encryptText(char *plainText, App *app) {
+    // typecasting char* to double = WARNING!!
+    return leaf_encrypt(1234, plainText);
+}
+
+char* decryptText(char *cipherText, App *app) {
+    /// TODO: Change app->encryptKey type or there will be messy errors!
+    return leaf_decrypt(1234, cipherText);
+}
+
+void textToFile(char *cipherText, char *fileLocation, App *app) {
+    FILE *save = fopen(fileLocation, "w");
+    fprintf(save, "%s\n%s", "E", cipherText);
+
+    // Close All Local Resources
+    fclose(save);
+}
 
 
 /// Helper Methods ///
 
-static void createRegAndLogIt() {
-    FILE *createIt = fopen(path, "w");
-    char result = createRegistryEntries(CONTEXTMENUITEM, app);
-    fprintf(createIt, "%c", result);
 
-    // Free up the resources
-    fclose(createIt);
-}
-
-static bool createRegistryEntries(char optionName[], App *app) {
+static char createRegistryEntries(char optionName[], App *app) {
     char createShell[] = "REG ADD HKCU\\SOFTWARE\\Classes\\.txt\\shell";
 
     char regWithOption[PATHSIZE];
@@ -188,7 +237,7 @@ static bool createRegistryEntries(char optionName[], App *app) {
     system(commandReg);
     system(defaultValue);
 
-    return true;
+    return 'T';
 }
 
 // TODO: Change the name of this method
@@ -196,8 +245,8 @@ static int isRegFileOK(App *app) {
     app->regPath = (char *) calloc(PATHSIZE, sizeof(char));
     snprintf(app->regPath, PATHSIZE, "%s\\%s.txt", app->homePath, REGFILECHECK);
 
-    if (access(path, "r") == F_OK) {
-        FILE *opened = fopen(path, "r");
+    if (access(app->regPath, "r") == F_OK) {
+        FILE *opened = fopen(app->regPath, "r");
         char input = (char) fgetc(input);
         if (input == 'T') {
             // Is true == 1?
@@ -215,12 +264,42 @@ static int isRegFileOK(App *app) {
 
 }
 
+static char* getFirstCharFromFile(char *fileLocation, App *app) {
+    if (access(fileLocation, F_OK) == F_OK) {
+        char *input = (char *) calloc(2, sizeof(char));
+
+        app->plainText = fopen(fileLocation, "r+");
+        fgets(input, 2, app->plainText);
+        return input;
+    } else {
+        return NULL;
+    }
+}
+
+/**
+Check if fileLocation is NULL or not, if null, do the app's installation
+*/
+static bool isEncrypted(char *fileLocation, App *app) {
+    char *encryptionStatus = getFirstCharFromFile(fileLocation, app);
+    bool status = strcmp(encryptionStatus, "E\n") == 0;
+
+    // free resources
+    free(encryptionStatus);
+
+    return status;
+}
+
 /**
 Frees all the RAM held by the App data block.
 */
 void freeApp(App *app) {
     // I don't know if this works, but I will use it anyway
     free(app->homePath);
+    free(app->encryptKey);
+    free(app->homePath);
     free(app->logPath);
+    fclose(app->plainText);
+    free(app->regPath);
+
     free(app);
 }
